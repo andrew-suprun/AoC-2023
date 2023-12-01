aea317e055edf19d789de39717c06fcde5b6f47c
Andrew Suprun <andrew.suprun@gmail.com>
2023-12-01 13:47:57 -0500

    no zeros in input

--- src/main/kotlin/day01/day01.kt
+++ src/main/kotlin/day01/day01.kt
@@ -22,7 +22,7 @@ fun findDigits(line: String, digits: Map<String, Int>): List<Int> {
     return result
 }
 
-val digitsPart1 = mapOf("0" to 0, "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9)
+val digitsPart1 = mapOf("1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9)
 val digitsPart2 = mapOf(
-        "zero" to 0, "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9,
-        "0" to 0, "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9)
+        "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9,
+        "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9)
