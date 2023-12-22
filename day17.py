7f9fd60b2d3b0059c14e1886631fecd97475c52c
Andrew Suprun <andrew.suprun@gmail.com>
2023-12-22 12:39:01 -0500

    Day 17: formatting

--- day17.py
+++ day17.py
@@ -14,17 +14,18 @@ def step(city_map, cursors, cursor, step_range, first_turn):
         dy, dx = 0, 1
     else:
         dy, dx = 1, 0
+
     new_loss = block[dir]
     for i in step_range:
         new_y, new_x = y + dy * i, x + dx * i
         if new_y < 0 or new_y >= max_y or new_x < 0 or new_x >= max_x:
             break
+
         new_block = city_map[new_y][new_x]
         new_loss += new_block[2]
         if new_block[new_dir] == 0 or new_block[new_dir] > new_loss:
             if abs(i) >= first_turn:
                 new_block[new_dir] = new_loss
-
                 cursors.append((new_y, new_x, new_dir))
 
 
@@ -39,6 +40,7 @@ def run(range1, range2, first_turn):
         cursor = cursors.popleft()
         step(city_map, cursors, cursor, range1, first_turn)
         step(city_map, cursors, cursor, range2, first_turn)
+
     exit_block = city_map[-1][-1]
     print(min(exit_block[0], exit_block[1]))
 
