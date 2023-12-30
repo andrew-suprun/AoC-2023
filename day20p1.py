8b497c742a63c3aef07e5bc2aaf71519ab275602
Andrew Suprun <andrew.suprun@gmail.com>
2023-12-30 16:20:22 -0500

    Day 20, part 1: refactor

--- day20p1.py
+++ day20p1.py
@@ -8,15 +8,13 @@ class Network(Protocol):
 
 
 class Module(Protocol):
-    targets: list[str]
-
-    def trigger(self, source: str, level: bool, network: Network):
+    def trigger(self, source: str, level: bool):
         ...
 
 
 class ModuleNetwork:
-    def __init__(self, modules: dict[str, Module]):
-        self.modules = modules
+    def __init__(self):
+        self.modules = dict[str, Module]()
         self.signals = deque[tuple[str, str, bool]]()
         self.low_signals = 0
         self.high_signals = 0
@@ -33,90 +31,96 @@ class ModuleNetwork:
         self.send("button", "broadcaster", False)
         while self.signals:
             signal = self.signals.popleft()
-            m = self.modules.get(signal[1])
-            if m:
-                m.trigger(signal[0], signal[2], self)
+            module = self.modules.get(signal[1])
+            if module:
+                module.trigger(signal[0], signal[2])
 
 
 class FlipFlop:
     targets: list[str]
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, network: Network):
         self.name = name
+        self.network = network
         self.state = False
 
-    def trigger(self, _source: str, level: bool, network: Network):
+    def trigger(self, _source: str, level: bool):
         if not level:
             self.state = not self.state
             for out in self.targets:
-                network.send(self.name, out, self.state)
+                self.network.send(self.name, out, self.state)
 
     def __repr__(self):
         return f"FlipFlop: state={self.state} out={self.targets}"
 
 
-class Conj(Module):
+class Conj:
     targets: list[str]
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, network: Network):
         self.name = name
+        self.network = network
         self.state = dict[str, bool]()
 
-    def trigger(self, source: str, level: bool, network: Network):
+    def trigger(self, source: str, level: bool):
         self.state[source] = level
         level = False if all(self.state.values()) else True
         for out in self.targets:
-            network.send(self.name, out, level)
+            self.network.send(self.name, out, level)
 
     def __repr__(self):
         return f"Conj: state={self.state} out={self.targets}"
 
 
-class Broadcaster(Module):
+class Broadcaster:
     targets: list[str]
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, network: Network):
         self.name = name
+        self.network = network
 
-    def trigger(self, _source: str, _level: bool, network: Network):
+    def trigger(self, _source: str, _level: bool):
         for target in self.targets:
-            network.send("broadcaster", target, False)
+            self.network.send("broadcaster", target, False)
 
     def __repr__(self):
         return f"Broadcaster: out={self.targets}"
 
 
-def parse_input() -> dict[str, Module]:
-    modules = dict[str, Module]()
+def parse_input() -> ModuleNetwork:
+    network = ModuleNetwork()
     with open("input/day20.data") as f:
         for line in f.readlines():
             full_name, output_str = line.strip().split(" -> ")
-            targets: list[str] = output_str.split(", ")
-            module = modules.get(full_name[1:])
+            module = network.modules.get(full_name[1:])
             if not module:
                 if full_name[0] == "%":
-                    module = FlipFlop(full_name[1:])
-                    modules[full_name[1:]] = module
+                    module = FlipFlop(full_name[1:], network)
+                    network.modules[full_name[1:]] = module
                 elif full_name[0] == "&":
-                    module = Conj(full_name[1:])
-                    modules[full_name[1:]] = module
+                    module = Conj(full_name[1:], network)
+                    network.modules[full_name[1:]] = module
                 else:
-                    module = Broadcaster(full_name)
-                    modules[full_name] = module
-            module.targets = targets
+                    module = Broadcaster(full_name, network)
+                    network.modules[full_name] = module
+            module.targets = output_str.split(", ")
 
-    for name, m in modules.items():
+    for name, m in network.modules.items():
         for out in m.targets:
-            receiver = modules.get(out)
+            receiver = network.modules.get(out)
             if type(receiver) is Conj:
                 receiver.state[name] = False
-    return modules
+    return network
 
 
-network = ModuleNetwork(parse_input())
+def part1():
+    network = parse_input()
 
-for _ in range(1000):
-    network.run()
+    for _ in range(1000):
+        network.run()
 
-print(f"Part 1: {network.low_signals * network.high_signals}")
+    print(f"Part 1: {network.low_signals * network.high_signals}")
+
+
+part1()
 # Part 1: 763500168
